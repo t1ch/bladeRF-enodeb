d55bff81dbf0ca47e32373f7284bf7b93d58a53f
Tich Kadzinga <tichaona@kadzinga.com>
2023-02-28 17:10:29 +0100

    Test Leds

--- fpga/vhdl/enodeb_top.vhd
+++ fpga/vhdl/enodeb_top.vhd
@@ -8,47 +8,56 @@ library nuand;
 
 entity enodeb_top is
   port (
-    tx_clock           :   in  std_logic ;
---    tx_reset           :   in  std_logic ;
---    tx_enable          :   in  std_logic ;
-
---    packet_en          :   in  std_logic ;
-
---    tx_packet_control  :   in      packet_control_t ;
---    tx_packet_empty    :   in      std_logic ;
---    tx_packet_ready    :   out     std_logic ;
-    tx_led_1           :   out     std_logic ;
-    tx_led_2           :   out     std_logic ;
-    tx_led_3           :   out     std_logic
+    tx_clock                :   in      std_logic ;
+    tx_reset                :   in      std_logic;
+    tx_packet_empty         :   in      std_logic;
+    tx_packet_control       :   in      packet_control_t;
+    tx_led_1                :   out     std_logic ;
+    tx_led_2                :   out     std_logic ;
+    tx_led_3                :   out     std_logic
   ) ;
 end enodeb_top ;
 
 architecture simple of enodeb_top is
-  signal tx_led_reg_1  : std_logic;
-  signal tx_led_reg_2  : std_logic;
-  signal tx_led_reg_3  : std_logic;
-
+  signal packet_counter : natural range 0 to 3 := 0;
 begin
+
   blink_leds : process (tx_clock)
-    variable counter : natural range 0 to 57_600_000 := 57_600_000;
   begin
     if (rising_edge(tx_clock)) then
-        counter := counter - 1;
-        if (counter = 0) then
-            counter := 57_600_000;
-        elsif (counter < 19_200_000) then
+        if (packet_counter = 0) then
+            tx_led_2 <= '1';
+            tx_led_1 <= '1';
+            tx_led_3 <= '1';
+        elsif (packet_counter = 1) then
             tx_led_2 <= '0';
             tx_led_1 <= '1';
             tx_led_3 <= '1';
-        elsif (counter < 38_400_000) then
-            tx_led_2 <= '1';
+        elsif (packet_counter = 2) then
+            tx_led_2 <= '0';
             tx_led_1 <= '0';
             tx_led_3 <= '1';
-        else
-            tx_led_2 <= '1';
-            tx_led_1 <= '1';
+        elsif (packet_counter = 3) then
+            tx_led_2 <= '0';
+            tx_led_1 <= '0';
             tx_led_3 <= '0';
         end if;
     end if;
   end process;
+
+
+  receive_packets : process(tx_clock)
+  begin
+    if (rising_edge(tx_clock)) then
+      if tx_packet_empty = '0' then
+        if(packet_counter = 3) then
+          packet_counter <= 0;
+        end if;
+        if(tx_packet_control.data_valid = '1') then
+        packet_counter <= packet_counter + 1;
+        end if;
+      end if;
+    end if;
+  end process;
+
 end simple;
